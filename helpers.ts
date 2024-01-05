a69cfdb1da479be16a125de2a155342df71af4a4
Ivan <fedorchuk.kp93@gmail.com>
2024-01-05 16:07:27 +0200

    Added helpers module

---
+++ helpers.ts
@@ -0,0 +1,15 @@
+export function getFileNames() {
+  return [''];
+}
+
+export function scanFile(index: any[], filename: string, file_position: number) {
+  return index;
+}
+
+export function wordsToFormat(words: string[]) {
+  return [''];
+}
+
+export function getWordCountsInFiles(word: string) {
+  return [''];
+}
