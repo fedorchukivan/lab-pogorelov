61c16d71153fc88faf9c4e7cf52ec9cf6ea03676
Ivan <fedorchuk.kp93@gmail.com>
2024-01-07 21:34:21 +0200

    Added index creation handler implementation

--- handlers.ts
+++ handlers.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express";
 import config from "./config";
+import { getFileNames, scanFile } from "./helpers";
+import { IndexList } from "./linked-list";
+import fs from 'fs';
+import path from "path";
 
 export function indexCreationHandler(req: Request, res: Response) {
-  console.log('Index creating for files from ' + config.files_dir_path + ' directory');
+  const files = getFileNames();
+  let index = new IndexList();
+  for (let i = 0; i < files.length; i++) {
+    scanFile(index, files[i], i);
+  }
+  const indexJSON = JSON.stringify(index);
+  fs.writeFileSync(path.resolve(config.index_path), indexJSON);
+  res.status(200).send('Index file was created successfully!');
 }
 
 export function queryHandler(req: Request, res: Response) {
