85a13a364e4daf03d97c2407afe6430e9a4a6a72
Ivan <fedorchuk.kp93@gmail.com>
2024-01-07 17:57:34 +0200

    Added prototype for index list unit tests

---
+++ tests/unit/linked-list.test.ts
@@ -0,0 +1,12 @@
+import { Node } from "../../linked-list";
+
+describe('Index list classes', () => {
+
+  describe('Index list node class', () => {
+    
+  });
+
+  describe('Index list class', () => {
+
+  });
+});
