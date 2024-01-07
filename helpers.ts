b70ed3450e1c02d3b1c0ab0200f0e6f5bd763309
Ivan <fedorchuk.kp93@gmail.com>
2024-01-07 16:25:58 +0200

    Modified words formatting function

--- helpers.ts
+++ helpers.ts
@@ -11,9 +11,15 @@ export function scanFile(index: any[], filename: string, file_position: number) 
   return index;
 }
 
+function leaveOnlyLetters(word: string) {
+  return word.match(/[A-Za-z]/g)?.join('');
+}
+
 export function wordsToFormat(words: string[]) {
-  const res = words.map(w => String(w.match(/[A-Za-z]/g)?.join('')));
-  return res.map(w => w.toLowerCase());
+  const res = words
+                .map(w => leaveOnlyLetters(w))
+                .filter(w => w !== undefined);
+  return res.map(w => String(w).toLowerCase());
 }
 
 export function getWordCountsInFiles(word: string) {
