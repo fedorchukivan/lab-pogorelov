f0d78a75c8afeec422ad14a1c4e96250114c7cab
Ivan <fedorchuk.kp93@gmail.com>
2024-01-08 14:42:28 +0200

    Modified index creating route integration tests

--- tests/integration/routes.test.ts
+++ tests/integration/routes.test.ts
@@ -11,12 +11,16 @@ describe('API Routes', () => {
     });
     it('should add relevant files to artifacts directory', () => {
       const files = fs.readdirSync(config.files_loc + 'artifacts');
-      expect(files).toContain(['filenames.json', 'test-index.json']);
+      expect(files).toContain(['test-filenames.json', 'test-index.json']);
     });
     it('should have appropriate content in resulting files', () => {
       const buff = fs.readFileSync(config.files_loc + 'artifacts/test-filenames.json').toString();
       const files = JSON.parse(buff);
       expect(files.files).toEqual(['file1.txt', 'file2.txt', 'file3.txt']);
+      const index_buff = fs.readFileSync(config.files_loc + 'artifacts/test-index.json').toString();
+      const index = JSON.parse(index_buff);
+      expect(index[0]).toBe({ word: 'chamber', freq: [3,1,1] });
+      expect(index[5]).toBe({ word: 'talking', freq: [1] });
     });
   });
 
